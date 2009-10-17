fc7dff9a421ffe26dede3c1d8d32ad0cd3b6e0b4
Krzesimir Nowak <qdlacz@gmail.com>
2009-10-17 21:16:59 +0200

    Partially remove GGraph stuff.
    * configure.ac: Makefile in examples/graph won't be created now.
    * examples/Makefile.am: Removed graph from SUBDIRS.
    * examples/graph/Makefile.am:
    * examples/graph/graph.c: Deleted.
    * src/libggraph.h: Throw out all GGraph includes and put in all
    GSGraph ones.
    * src/libggraph/Makefile.am: Throw out all GGraph files from
    building.

--- examples/graph/graph.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-  printf("This will be graph example.\n");
-  return 0;
-}
--- src/libggraph.h
+++ src/libggraph.h
@@ -3,17 +3,12 @@
 
 #define _LIB_G_GRAPH_H_INSIDE_
 
-#include <libggraph/ggraphtypedefs.h>
 #include <libggraph/gsgraphtypedefs.h>
-
-#include <libggraph/ggraph.h>
-#include <libggraph/ggraphedge.h>
-#include <libggraph/ggrapharray.h>
-#include <libggraph/ggraphedgearray.h>
 
 #include <libggraph/gsgraph.h>
 #include <libggraph/gsgrapharray.h>
+#include <libggraph/gsgraphdatapair.h>
 
-#undef _LIB_G_GRAPH_INSIDE_
+#undef _LIB_G_GRAPH_H_INSIDE_
 
-#endif // _LIB_G_GRAPH_H_
+#endif /* _LIB_G_GRAPH_H_ */
