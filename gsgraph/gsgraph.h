264dab1ce3b04a100a9c385b94f628ac59c3c65f
Krzesimir Nowak <qdlacz@gmail.com>
2009-10-20 12:33:16 +0200

    Relicense to LGPLv3 or later.
    * COPYING.LESSER: new file with LGPLv3 text, which is addition to
    existing LGPLv3 text in COPYING.
    * gsgraph/gsgraph*: Add copyright and license header to all sources
    and header files.

--- gsgraph/gsgraph.h
+++ gsgraph/gsgraph.h
@@ -1,7 +1,26 @@
-#ifndef _LIB_G_GRAPH_H_
-#define _LIB_G_GRAPH_H_
+/*
+ * Copyright (C) 2009 Krzesimir Nowak <qdlacz@gmail.com>
+ *
+ * This file is part of libggraph.
+ *
+ * libggraph is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libggraph is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libggraph.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#define _LIB_G_GRAPH_H_INSIDE_
+#ifndef _G_SGRAPH_H_
+#define _G_SGRAPH_H_
+
+#define _G_SGRAPH_H_INSIDE_
 
 #include <gsgraph/gsgraphtypedefs.h>
 
@@ -9,6 +28,6 @@
 #include <gsgraph/gsgraphnodearray.h>
 #include <gsgraph/gsgraphdatapair.h>
 
-#undef _LIB_G_GRAPH_H_INSIDE_
+#undef _G_SGRAPH_H_INSIDE_
 
-#endif /* _LIB_G_GRAPH_H_ */
+#endif /* _G_SGRAPH_H_ */
--- gsgraph/gsgraphnode.h
+++ gsgraph/gsgraphnode.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2009 Krzesimir Nowak <qdlacz@gmail.com>
+ *
+ * This file is part of libggraph.
+ *
+ * libggraph is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libggraph is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libggraph.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #if defined(G_DISABLE_SINGLE_INCLUDES) && !defined (_G_SGRAPH_H_INSIDE_) && !defined (_LIB_G_GRAPH_COMPILATION_)
 #error "Only <gsgraph/gsgraph.h> can be included directly."
 #endif
