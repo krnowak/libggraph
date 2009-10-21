b4d3cd43da82cb4f24fc561d933f1e0abf68fb2a
Krzesimir Nowak <qdlacz@gmail.com>
2009-10-21 12:20:15 +0200

    Adapt GSEGraphEdgeArray.
    * junk/gsegraphedgearray.c:
    * junk/gsegraphedgearray.h: Renamed GGraph* types to GSEGraph*
    ones, g_graph_* functions to g_segraph_* ones. Adapted docs.

--- junk/gsegraphedgearray.c
+++ junk/gsegraphedgearray.c
@@ -1,11 +1,30 @@
-#include "ggraphedgearray.h"
+/*
+ * Copyright (C) 2009 Krzesimir Nowak <qdlacz@gmail.com>
+ *
+ * This file is part of libggraph.
+ *
+ * libggraph is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libggraph is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libggraph.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "gsegraphedgearray.h"
 
 /**
  * SECTION: ggraphedgearray
  * @title: Array of edges to graphs
- * @short_description: arrays of pointers to #GGraphEdge, which grow
+ * @short_description: arrays of pointers to #GSEGraphEdge, which grow
  * automatically as new elements are added
- * @include: libggraph.h
+ * @include: gsgraph/gsegraph.h
  * @see_also: #GPtrArray
  *
  * Array of edges is a type-safe wrapper around #GPtrArray.
@@ -14,74 +33,73 @@
 /* functions definitions */
 
 /**
- * g_graph_edge_array_new:
+ * g_segraph_edge_array_new:
  *
- * Convenience type-safe function, creating a new #GGraphEdgeArray.
+ * Convenience type-safe function, creating a new #GSEGraphEdgeArray.
  *
  * See also: g_ptr_array_new()
  *
  * Returns: newly created array.
  */
-GGraphEdgeArray*
-g_graph_edge_array_new(void)
+GSEGraphEdgeArray*
+g_segraph_edge_array_new(void)
 {
   return g_ptr_array_new();
 }
 
 /**
- * g_graph_edge_array_sized_new:
+ * g_segraph_edge_array_sized_new:
  * @reserved_size: size of array.
  *
- * Convenience type-safe function, creating a new #GGraphEdgeArray with given
+ * Convenience type-safe function, creating a new #GSEGraphEdgeArray with given
  * size.
  *
  * See also: g_ptr_array_sized_new()
  *
  * Returns: newly created array.
  */
-GGraphEdgeArray*
-g_graph_edge_array_sized_new(guint reserved_size)
+GSEGraphEdgeArray*
+g_segraph_edge_array_sized_new(guint reserved_size)
 {
   return g_ptr_array_sized_new(reserved_size);
 }
 
 /**
- * g_graph_edge_array_add:
+ * g_segraph_edge_array_add:
  * @array: an array.
- * @edge: #GGraphEdge to add to array.
+ * @edge: #GSEGraphEdge to add to array.
  *
  * Convenience type-safe function adding @edge to @array.
  *
  * See also: g_ptr_array_add()
  */
 void
-g_graph_edge_array_add(GGraphEdgeArray* array,
-                       GGraphEdge* edge)
+g_segraph_edge_array_add(GSEGraphEdgeArray* array,
+                         GSEGraphEdge* edge)
 {
   g_ptr_array_add(array, edge);
 }
 
 /**
- * g_graph_edge_array_remove:
+ * g_segraph_edge_array_remove:
  * @array: an array.
  * @edge: edge to be removed from @array.
  *
- * Convenience type-safe function removing #GSGraph from #GSGraphArray,
- * preserving order.
+ * Convenience type-safe function removing @edge from @array, preserving order.
  *
  * See also: g_ptr_array_remove()
  *
  * Returns: %TRUE if @edge was removed.
  */
 gboolean
-g_graph_edge_array_remove(GGraphEdgeArray* array,
-                          GGraphEdge* edge)
+g_segraph_edge_array_remove(GSEGraphEdgeArray* array,
+                            GSEGraphEdge* edge)
 {
   return g_ptr_array_remove(array, edge);
 }
 
 /**
- * g_graph_edge_array_free:
+ * g_segraph_edge_array_free:
  * @array: an array to free.
  * @free_segment: %TRUE if the actual pointer array has to be freed.
  *
@@ -89,12 +107,12 @@ g_graph_edge_array_remove(GGraphEdgeArray* array,
  *
  * See also: g_ptr_array_free()
  *
- * Returns: %NULL if @free_segment was %TRUE, otherwise pointer to array of
- * #GGraphEdge is returned - it should be freed with g_free().
+ * Returns: %NULL if @free_segment was %TRUE, otherwise pointer to data of
+ * @array is returned - it should be freed with g_free().
  */
-GGraphEdge**
-g_graph_edge_array_free(GGraphEdgeArray* array,
-                        gboolean free_segment)
+GSEGraphEdge**
+g_segraph_edge_array_free(GSEGraphEdgeArray* array,
+                          gboolean free_segment)
 {
-  return (GGraphEdge**)g_ptr_array_free(array, free_segment);
+  return (GSEGraphEdge**)g_ptr_array_free(array, free_segment);
 }
--- junk/gsegraphedgearray.h
+++ junk/gsegraphedgearray.h
@@ -1,62 +1,67 @@
-#if defined(G_DISABLE_SINGLE_INCLUDES) && !defined (_LIB_G_GRAPH_H_INSIDE_) && !defined (_LIB_G_GRAPH_COMPILATION_)
-#error "Only <libggraph.h> can be included directly."
+/*
+ * Copyright (C) 2009 Krzesimir Nowak <qdlacz@gmail.com>
+ *
+ * This file is part of libggraph.
+ *
+ * libggraph is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libggraph is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libggraph.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if defined(G_DISABLE_SINGLE_INCLUDES) && !defined (_G_SEGRAPH_H_INSIDE_) && !defined (_LIB_G_GRAPH_COMPILATION_)
+#error "Only <gsgraph/gsegraph.h> can be included directly."
 #endif
 
-#ifndef _G_GRAPH_EDGE_ARRAY_H_
-#define _G_GRAPH_EDGE_ARRAY_H_
+#ifndef _G_SEGRAPH_EDGE_ARRAY_H_
+#define _G_SEGRAPH_EDGE_ARRAY_H_
 
 #include <glib.h>
-#include <libggraph/ggraphtypedefs.h>
-#include <libggraph/ggraphedge.h>
+#include <gsgraph/gsegraphtypedefs.h>
+#include <gsgraph/gsegraphedge.h>
 
 G_BEGIN_DECLS
 
-/**
- * GGraphEdgeArray:
- *
- * Contains the public fields of an array.
- */
+GSEGraphEdgeArray*
+g_segraph_edge_array_new(void);
 
-GGraphEdgeArray*
-g_graph_edge_array_new(void);
-/*
-#define g_graph_edge_array_new() \
-((GGraphEdgeArray*)(g_ptr_array_new()))
-*/
-
-GGraphEdgeArray*
-g_graph_edge_array_sized_new(guint reserved_size);
+GSEGraphEdgeArray*
+g_segraph_edge_array_sized_new(guint reserved_size);
 
 /**
- * g_graph_edge_array_index:
+ * g_segraph_edge_array_index:
  * @array: an array.
- * @index: index of the #GGraphEdge to return.
+ * @index: index of the #GSEGraphEdge to return.
  *
- * Convenience type-safe macro, returning a #GGraphEdge at given index.
+ * Convenience type-safe macro, returning a #GSEGraphEdge at given index.
  *
  * See also: g_ptr_array_index()
  *
- * Returns: #GGraphEdge at given index.
+ * Returns: #GSEGraphEdge at given index.
  */
-#define g_graph_edge_array_index(array, index) \
-((GGraphEdge*)(g_ptr_array_index((array), (index))))
+#define g_segraph_edge_array_index(array, index) \
+((GSEGraphEdge*)(g_ptr_array_index((array), (index))))
 
 void
-g_graph_edge_array_add(GGraphEdgeArray* array,
-                       GGraphEdge* edge);
+g_segraph_edge_array_add(GSEGraphEdgeArray* array,
+                         GSEGraphEdge* edge);
 
 gboolean
-g_graph_edge_array_remove(GGraphEdgeArray* array,
-                          GGraphEdge* edge);
+g_segraph_edge_array_remove(GSEGraphEdgeArray* array,
+                            GSEGraphEdge* edge);
 
-GGraphEdge**
-g_graph_edge_array_free(GGraphEdgeArray* array,
-                        gboolean free_segment);
-/*
-#define g_graph_edge_array_free(array, free_segment) \
-((GGraphEdge**)(g_ptr_array_free((array), (free_segment))))
-*/
+GSEGraphEdge**
+g_segraph_edge_array_free(GSEGraphEdgeArray* array,
+                          gboolean free_segment);
 
 G_END_DECLS
 
-#endif /* _G_GRAPH_EDGE_ARRAY_H_ */
+#endif /* _G_SEGRAPH_EDGE_ARRAY_H_ */
