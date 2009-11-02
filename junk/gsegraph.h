6782750378e8c1bd808a465978e8e153a9be6e3f
Krzesimir Nowak <qdlacz@gmail.com>
2009-11-02 23:06:46 +0100

    Add main header file.
    * junk/gsegraph.h: Main header file including all other GSEGraph
    headers.

---
+++ junk/gsegraph.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2009 Krzesimir Nowak <qdlacz@gmail.com>
+ *
+ * This file is part of libggraph.
+ *
+ * libggraph is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libggraph is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libggraph.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _G_SEGRAPH_H_
+#define _G_SEGRAPH_H_
+
+#define _G_SEGRAPH_H_INSIDE_
+
+#include <gsgraph/gsegraphtypedefs.h>
+
+#include <gsgraph/gsegraphnode.h>
+#include <gsgraph/gsegraphedge.h>
+#include <gsgraph/gsegraphdatatriplet.h>
+#include <gsgraph/gsegraphwhole.h>
+#include <gsgraph/gsegraphenums.h>
+
+#undef _G_SEGRAPH_H_INSIDE_
+
+#endif /* _G_SEGRAPH_H_ */
